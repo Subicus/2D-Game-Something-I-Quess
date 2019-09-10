f0f8c95b3cfc6d98f36272c7d7ee97348fe9b9ec
Subicus <dulesuba91@gmail.com>
2019-09-10 19:10:21 +0200

    Sword Script ReVamp

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+
+{
+    public GameObject Player;
+    public Vector3 offset;
+    public float swordDistance = 1;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 characterPos = transform.position;
+        Vector2 dir = mousePos - characterPos;
+        dir = dir.normalized;
+        Vector2 swordPos = characterPos + swordDistance * dir;
+
+    }
+
+   void OnDrawGizmos()
+          {
+             if (!Application.isPlaying) return;
+             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+             Vector2 characterPos = transform.position;
+             Gizmos.DrawSphere(mousePos, 1);
+             Vector2 dir = mousePos - characterPos;
+             dir = dir.normalized;
+             Vector2 swordPos = characterPos + swordDistance*dir;
+             Gizmos.DrawLine(characterPos, swordPos);
+         }
+
+}
